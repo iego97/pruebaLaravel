f9d2589c9fb4a3037382b665eb9846ccd6a20dc9
Alumno <ulsa@iMacD115.local>
2019-01-14 18:50:30 -0700

    prueba

---
+++ resources/views/hola.blade.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hola</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+<h1> HOLA A TODOS <h1>
+<p> Esto es una pagina html </p>
+
+</body>
+</html>
